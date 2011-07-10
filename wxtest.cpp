1556ef5ed08da2748d4cfd492143acd08071febe
Nicolay Korslund <korslund@gmail.com>
2011-07-10 12:15:08 +0200

    EOS - quit button

--- wxtest.cpp
+++ wxtest.cpp
@@ -6,10 +6,23 @@ using namespace std;
 class Simple : public wxFrame
 {
 public:
-    Simple(const wxString& title)
-      : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(250, 150))
+  Simple(const wxString& title)
+    : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(250, 150))
   {
     Centre();
+
+    wxPanel *panel = new wxPanel(this, wxID_ANY);
+    wxButton *button = new wxButton(panel, wxID_EXIT, wxT("Quit"),
+                                    wxPoint(20, 20));
+
+    Connect(wxID_EXIT, wxEVT_COMMAND_BUTTON_CLICKED,
+            wxCommandEventHandler(Simple::onQuit));
+    button->SetFocus();
+  }
+
+  void onQuit(wxCommandEvent &event)
+  {
+    Close(true);
   }
 };
 
