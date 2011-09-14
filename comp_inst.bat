fd282dbac4a70391123fd6c98f10d09de50b5fc4
Nicolay Korslund <korslund@gmail.com>
2011-09-14 18:23:54 +0200

    The installer looks to be surprisingly easy to make

--- comp_inst.bat
+++ comp_inst.bat
@@ -1,1 +1,1 @@
-g++ win32_installer.cpp -o ogl_setup.exe -Wl,--subsystem,windows -I../mingw-div/include/ -L../boost -lboost_filesystem -lboost_system
+g++ win32_installer.cpp -o setup_stub.exe -Wl,--subsystem,windows -I../mingw-div/include/ -L../boost -lboost_filesystem -lboost_system -I../zziplib -I../zlib -L../zziplib/zzip -lzzip -L../zlib-1.2.5 -lz
--- win32_installer.cpp
+++ win32_installer.cpp
@@ -1,26 +1,57 @@
 #include <windows.h>
+#include <shlobj.h>
+#include <shellapi.h>
 #include <boost/filesystem.hpp>
 #include <string>
+#include "unzip.hpp"
+
+namespace fs = boost::filesystem;
 
 TCHAR pathbuf[MAX_PATH];
 
-std::string getBinPath(const std::string &appName)
+fs::path getBinPath(const std::string &appName)
 {
   SHGetFolderPath(NULL, CSIDL_LOCAL_APPDATA, NULL, 0, pathbuf);
-
-  boost::filesystem::path base = pathbuf;
-  return (base / appName / "bin").string();
+  fs::path base = pathbuf;
+  return base / appName / "bin";
 }
 
 std::string getExe()
 {
   GetModuleFileName(NULL, pathbuf, MAX_PATH);
+  return std::string(pathbuf);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                    LPSTR lpCmdLine, INT nCmdShow)
 {
+  fs::path bin = getBinPath("tiggit");
   std::string exe = getExe();
-  std::string dir = getBinPath("tiggit");
-  MessageBox(0, dir.c_str(), exe.c_str(), MB_ICONINFORMATION);
+
+  // Make sure bin/ exists
+  fs::create_directories(bin);
+
+  // TODO: Extract embedded ZIP file and store the filename here
+  std::string zip = "tmp.zip";
+
+  // Unpack the contents into the bin/ folder
+  try
+    {
+      Unzip z;
+      z.unpack(zip, bin.string());
+    }
+  catch(std::exception &e)
+    {
+      MessageBox(NULL, e.what(), "Error", MB_ICONERROR);
+      return 1;
+    }
+
+  // Kill the zip file
+  remove(zip);
+
+  // Run the main program
+  ShellExecute(NULL, "open", (bin/"tiggit.exe").string().c_str(),
+               NULL, NULL, SW_SHOW);
+
+  return 0;
 }
